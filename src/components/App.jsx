7977b6eeb1331e4b44301a2adf273eed69d04b79
Ваня <7328652@gmail.com>
2023-10-20 03:51:17 +0300

    go

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,7 +20,6 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
         </Route>
       </Routes>
     </>
